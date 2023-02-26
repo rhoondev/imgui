e63cc96516c206af10036d9762a23a447d00fc03
rhoondev <rowan.efaulkner@gmail.com>
2023-02-26 00:44:30 -0500

    Converted Sapphire from dynamic to static library

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,8 @@
 project "ImGui"
 	kind "StaticLib"
 	language "C++"
+    cppdialect "C++20"
+    staticruntime "on"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -22,14 +24,10 @@ project "ImGui"
 
 	filter "system:windows"
 		systemversion "latest"
-		cppdialect "C++20"
-		staticruntime "On"
 
 	filter "system:linux"
 		pic "On"
 		systemversion "latest"
-		cppdialect "C++20"
-		staticruntime "On"
 
 	filter "configurations:Debug"
 		runtime "Debug"
